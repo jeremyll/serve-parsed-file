b0fee1723fdbde3f6cb750a2656fe2b39e3d3992
Jeremy Llewellyn <jeremy.llewellyn@gmail.com>
2021-02-12 21:19:35 -0800

    Remove unused backend sorting

--- server.py
+++ server.py
@@ -10,9 +10,6 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main_page():
-    sort_by = request.args.get('sort_by', None)
-    print(sort_by)
-
     rows = parse_file(FILE_PATH)
     headers = rows[0].keys()
 
